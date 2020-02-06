8289b6409b35d0a3ed47df4c2267d9379974879c
The Doctor <dawid.janik95@gmail.com>
2020-02-06 16:19:25 +0100

    Refactor services.

--- src/Services/CampaignDraftService.php
+++ src/Services/CampaignDraftService.php
@@ -1,23 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mailjet\LaravelMailjet\Services;
 
-use \Mailjet\Resources;
-use \Mailjet\Response;
-use Mailjet\LaravelMailjet\Services\MailjetService;
+use Mailjet\Response;
+use Mailjet\Resources;
 use Mailjet\LaravelMailjet\Model\CampaignDraft;
-use Mailjet\LaravelMailjet\Contracts\CampaignDraftContract;
 use Mailjet\LaravelMailjet\Exception\MailjetException;
+use Mailjet\LaravelMailjet\Contracts\CampaignDraftContract;
 
 /**
  * https://dev.mailjet.com/email-api/v3/campaigndraft/
- * CampaignDraft data. (list,view, create, update, delete,schedule,send ...)
  */
 class CampaignDraftService implements CampaignDraftContract
 {
     /**
-     * Mailjet client
-     * @var MailjetClient
+     * @var MailjetService
      */
     protected $mailjet;
 
@@ -27,32 +26,35 @@ class CampaignDraftService implements CampaignDraftContract
     }
 
     /**
-     * List campaigndraft resources available for this apikey
+     * List campaign draft resources available for this apikey.
+     *
      * @return array
+     * @throws \Mailjet\LaravelMailjet\Exception\MailjetException
      */
-    public function getAllCampaignDrafts(array $filters = null)
+    public function getAllCampaignDrafts(array $filters = null): array
     {
-        $response = $this->mailjet->get(Resources::$Campaigndraft,
-            ['filters' => $filters]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService :getAllCampaignDrafts() failed",
-                $response);
+        $response = $this->mailjet->get(Resources::$Campaigndraft, ['filters' => $filters]);
+
+        if (! $response->success()) {
+            throw new MailjetException(0, 'CampaignDraftService :getAllCampaignDrafts() failed', $response);
         }
 
         return $response->getData();
     }
 
     /**
-     * Access a given campaigndraft resource
-     * @param string $CampaignId
+     * Access a given CampaignDraft resource.
+     *
+     * @param string $id
+     *
      * @return array
      */
-    public function findByCampaignDraftId($CampaignId)
+    public function findByCampaignDraftId(string $id)
     {
         $response = $this->mailjet->get(Resources::$Campaigndraft,
-            ['id' => $CampaignId]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:findByCampaignDraftId() failed",
+            ['id' => $id]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:findByCampaignDraftId() failed',
                 $response);
         }
 
@@ -61,14 +63,15 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * create a new fresh CampaignDraft
+     *
      * @param Campaigndraft $campaignDraft
      */
     public function create(CampaignDraft $campaignDraft)
     {
         $response = $this->mailjet->post(Resources::$Campaigndraft,
             ['body' => $campaignDraft->format()]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:create() failed", $response);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:create() failed', $response);
         }
 
         return $response->getData();
@@ -76,15 +79,16 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Update one specific campaigndraft resource
-     * @param int $id
+     *
+     * @param int           $id
      * @param Campaigndraft $campaignDraft
      */
-    public function update($CampaignId, CampaignDraft $campaignDraft)
+    public function update($id, CampaignDraft $campaignDraft)
     {
         $response = $this->mailjet->put(Resources::$Campaigndraft,
-            ['id' => $CampaignId, 'body' => $campaignDraft->format()]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:update() failed", $response);
+            ['id' => $id, 'body' => $campaignDraft->format()]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:update() failed', $response);
         }
 
         return $response->getData();
@@ -92,15 +96,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Return the text and html contents of the campaigndraft
+     *
      * @param string $id
+     *
      * @return array
      */
-    public function getDetailContent($id)
+    public function getDetailContent(string $id)
     {
         $response = $this->mailjet->get(Resources::$CampaigndraftDetailcontent,
             ['id' => $id]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:getDetailContent failed",
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:getDetailContent failed',
                 $response);
         }
 
@@ -109,15 +115,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Creates the content of a  campaigndraft
+     *
      * @param string $id
+     *
      * @return array
      */
     public function createDetailContent($id, $contentData)
     {
         $response = $this->mailjet->post(Resources::$CampaigndraftDetailcontent,
             ['id' => $id, 'body' => $contentData]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:createDetailContent failed",
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:createDetailContent failed',
                 $response);
         }
 
@@ -126,15 +134,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Return the date of the scheduled sending of the campaigndraft
+     *
      * @param string Campaign $id
+     *
      * @return array
      */
-    public function getSchedule($CampaignId)
+    public function getSchedule(string $id)
     {
         $response = $this->mailjet->get(Resources::$CampaigndraftSchedule,
-            ['id' => $CampaignId]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:getSchedule failed",
+            ['id' => $id]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:getSchedule failed',
                 $response);
         }
 
@@ -143,16 +153,18 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Schedule when the campaigndraft will be sent
-     * @param string Campaign $id
-     * @param RFC3339 format "Y-m-d\TH:i:sP"  $date
+     *
+     * @param string $id
+     * @param string $date (RFC3339 format "Y-m-d\TH:i:sP")
+     *
      * @return array
      */
-    public function scheduleCampaign($CampaignId, $date)
+    public function scheduleCampaign(string $id, string $date)
     {
         $response = $this->mailjet->post(Resources::$CampaigndraftSchedule,
-            ['id' => $CampaignId, 'body' => $date]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:scheduleCampaign failed",
+            ['id' => $id, 'body' => $date]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:scheduleCampaign failed',
                 $response);
         }
 
@@ -161,16 +173,18 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Update the date when the campaigndraft will be sent
+     *
      * @param string Campaign $id
      * @param string Schedule $date
+     *
      * @return array
      */
-    public function updateCampaignSchedule($CampaignId, $date)
+    public function updateCampaignSchedule($id, $date)
     {
         $response = $this->mailjet->put(Resources::$CampaigndraftSchedule,
-            ['id' => $CampaignId, 'body' => $date]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:updateCampaignSchedule failed",
+            ['id' => $id, 'body' => $date]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:updateCampaignSchedule failed',
                 $response);
         }
 
@@ -179,15 +193,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Cancel a future sending
+     *
      * @param string Campaign $id
+     *
      * @return array
      */
-    public function removeSchedule($CampaignId)
+    public function removeSchedule(string $id)
     {
         $response = $this->mailjet->delete(Resources::$CampaigndraftSchedule,
-            ['id' => $CampaignId]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:removeSchedule failed",
+            ['id' => $id]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:removeSchedule failed',
                 $response);
         }
 
@@ -196,15 +212,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Send the campaign immediately
+     *
      * @param string Campaign $id
+     *
      * @return array
      */
-    public function sendCampaign($CampaignId)
+    public function sendCampaign(string $id)
     {
         $response = $this->mailjet->post(Resources::$CampaigndraftSend,
-            ['id' => $CampaignId]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:sendCampaign failed",
+            ['id' => $id]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:sendCampaign failed',
                 $response);
         }
 
@@ -213,15 +231,17 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * Return the status of a CampaignDraft
+     *
      * @param string Campaign $id
+     *
      * @return array
      */
-    public function getCampaignStatus($CampaignId)
+    public function getCampaignStatus(string $id)
     {
         $response = $this->mailjet->get(Resources::$CampaigndraftStatus,
-            ['id' => $CampaignId]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:getCampaignStatus failed",
+            ['id' => $id]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:getCampaignStatus failed',
                 $response);
         }
 
@@ -230,16 +250,18 @@ class CampaignDraftService implements CampaignDraftContract
 
     /**
      * An action to test a CampaignDraft.
+     *
      * @param string Campaign $id
      * @param array of  $recipients
+     *
      * @return array
      */
-    public function testCampaign($CampaignId, $recipients)
+    public function testCampaign($id, $recipients)
     {
         $response = $this->mailjet->post(Resources::$CampaigndraftTest,
-            ['id' => $CampaignId, 'body' => $recipients]);
-        if (!$response->success()) {
-            $this->throwError("CampaignDraftService:testCampaign failed",
+            ['id' => $id, 'body' => $recipients]);
+        if (! $response->success()) {
+            $this->throwError('CampaignDraftService:testCampaign failed',
                 $response);
         }
 
