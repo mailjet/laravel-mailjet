6aa6410f5477efe13af896ef6de11454b2690349
The Doctor <dawid.janik95@gmail.com>
2020-02-06 16:18:26 +0100

    Refactor facade.

--- src/Facades/Mailjet.php
+++ src/Facades/Mailjet.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Mailjet\LaravelMailjet\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 class Mailjet extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'Mailjet';
     }
