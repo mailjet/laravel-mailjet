93fb79c20f69e4945e466a1aba14df4c26f8af5d
Artem Bondarenko <abonda@rainpos.com>
2021-03-09 23:39:09 +0200

    fix

--- src/MailjetMailServiceProvider.php
+++ src/MailjetMailServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mailjet\LaravelMailjet;
 
+use Exception;
 use Illuminate\Mail\MailServiceProvider;
 use Mailjet\LaravelMailjet\Transport\MailjetTransport;
 use Swift_Events_SimpleEventDispatcher as EventDispatcher;
@@ -11,20 +12,36 @@ use Swift_Events_SimpleEventDispatcher as EventDispatcher;
 class MailjetMailServiceProvider extends MailServiceProvider
 {
     /**
-     * Extended register the Swift Transport instance.
+     * Extended register the Illuminate Mailer instance.
      *
      * @return void
      */
-    protected function registerSwiftTransport(): void
+    protected function registerIlluminateMailer(): void
     {
-        parent::registerSwiftTransport();
+        parent::registerIlluminateMailer();
 
-        app('swift.transport')->extend('mailjet', function () {
-            $config = $this->app['config']->get('services.mailjet', []);
-            $call = $this->app['config']->get('services.mailjet.transactional.call', true);
-            $options = $this->app['config']->get('services.mailjet.transactional.options', []);
+        try {
+            app('mail.manager')->extend('mailjet', function () {
+                return $this->mailjetTransport();
+            });
+        } catch (Exception $e) {
+            app('swift.transport')->extend('mailjet', function () {
+                return $this->mailjetTransport();
+            });
+        }
+    }
 
-            return new MailjetTransport(new EventDispatcher(), $config['key'], $config['secret'], $call, $options);
-        });
+    /**
+     * Return configured MailjetTransport.
+     *
+     * @return MailjetTransport
+     */
+    protected function mailjetTransport(): MailjetTransport
+    {
+        $config = $this->app['config']->get('services.mailjet', []);
+        $call = $this->app['config']->get('services.mailjet.transactional.call', true);
+        $options = $this->app['config']->get('services.mailjet.transactional.options', []);
+
+        return new MailjetTransport(new EventDispatcher(), $config['key'], $config['secret'], $call, $options);
     }
 }
